34a3305908a8204b3c4a386f8e577583f846f438
Igor Barbashov <bis200705@gmail.com>
2019-09-24 07:18:08 +0300

    zeros/feat: first trying

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function zeros(expression) {
-  // your solution
+// module.exports = function zeros(expression) {
+function zeros(expression) {
+  
+  const multuplies = expression.split('*');
+
+  multuplies.map(el => {
+    const oper = el.slice(-2);
+    if (oper === '!!') {
+      const base = +el.slice(0, -2);
+      return base % 2 === 0
+        ? howMuchTwo(base) : howMuchTwo(base);
+    }
+    return 
+  });
+  
+
+
 }
